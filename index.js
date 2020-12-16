341825c59f94fe4525d8d3e22b1f0f5fc1b97441
bharath123321 <bharathrajs22.bk@gmail.com>
2020-12-16 16:13:56 -0500

    set up static files access

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const app = express();
 
 const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
+
+app.use(express.static("./assets"));
 app.use(expressLayouts);
+
+// extract styled and scripts from sub page into layouts
+
+//app.set("layout extraStyles", true);
+//app.set("layout extraScripts", true);
 // Use express-roueter// routes
 app.use("/", require("./routes"));
 app.set("view engine", "ejs");
