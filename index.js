a0e45f98740b99da29609fa8ed825559160bb03e
bharath123321 <bharathrajs22.bk@gmail.com>
2020-12-18 22:34:06 -0500

    SCSS set up

--- index.js
+++ index.js
@@ -14,7 +14,17 @@ const passport = require("passport");
 const passportLocal = require("./config/passport-local-strategy");
 
 const MongoStore = require("connect-mongo")(session);
+const sassMiddleware = require("node-sass-middleware");
 
+app.use(
+  sassMiddleware({
+    src: "./assests/scss",
+    dest: "./assests/css/",
+    debug: true,
+    outputStyle: "extended",
+    prefix: "/css",
+  })
+);
 app.use(express.urlencoded());
 app.use(cookieParser());
 
